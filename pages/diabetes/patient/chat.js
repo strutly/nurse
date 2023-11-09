91616f24feb78c941b69da7b4194a9aa0199a634
strutly <290616329@qq.com>
2023-11-09 09:56:38 +0800

    对话修改下

--- pages/diabetes/patient/chat.js
+++ pages/diabetes/patient/chat.js
@@ -5,19 +5,30 @@ import CustomPage from '../../../CustomPage';
 CustomPage({
   data: {
     open: -1,
-    domain:Api.domain,
+    domain: Api.domain,
     chats: [],
     daysData: {}
   },
   onLoad(options) {
     that = this;
-    that.getList(1);
-    setTimeout(()=>{
-      that.setData({
-        intoView:"report0"
-      })
-    },1000);
+
   },
+  onReady() {
+    getApp().watch(function (value) {
+      if (value.login && value.auth) {
+        that.getList(1);
+        setTimeout(() => {
+          that.setData({
+            intoView: "report0"
+          })
+        }, 1000);
+      }else{
+        that.showTips("出错了~");
+      }
+    })
+  },
+
+
   async getList(pageNo) {
     let res = await Api.reportChat({
       pageNum: pageNo,
@@ -33,27 +44,27 @@ CustomPage({
       })
     }
   },
-  addList(){
+  addList() {
     console.log(1);
-    if(!that.data.last){
-      that.getList(that.data.pageNo+1);
+    if (!that.data.last) {
+      that.getList(that.data.pageNo + 1);
     }
-  },onPulling(e) {
+  }, onPulling(e) {
     console.log('onPulling:', e)
   },
 
   onRefresh() {
     if (this._freshing) return
     this._freshing = true
-    if(!that.data.last){
-      that.getList(that.data.pageNo+1);
+    if (!that.data.last) {
+      that.getList(that.data.pageNo + 1);
       setTimeout(() => {
         this.setData({
           triggered: false,
         })
         this._freshing = false
       }, 1000)
-    }    
+    }
   },
 
   onRestore(e) {
@@ -63,18 +74,18 @@ CustomPage({
   onAbort(e) {
     console.log('onAbort', e)
   },
-  async notice(e){
+  async notice(e) {
     console.log(e)
     let data = e.detail.value;
-    if(!data.advice) return that.showTips("请输入通知内容~");
-    let report = {type:4,status:0,createTime:Util.dateFormat(new Date().toUTCString(),"yyyy-MM-dd hh:mm:ss"),patientId:data.patientId,content:{advice:data.advice}}
+    if (!data.advice) return that.showTips("请输入通知内容~");
+    let report = { type: 4, status: 0, createTime: Util.dateFormat(new Date().toUTCString(), "yyyy-MM-dd hh:mm:ss"), patientId: data.patientId, content: { advice: data.advice } }
     let res = await Api.addReport(report);
-    if(res.code==0){
-      that.showTips("发送成功","success");
+    if (res.code == 0) {
+      that.showTips("发送成功", "success");
       let chats = that.data.chats;
       chats.unshift(report)
       that.setData({
-        chats:chats
+        chats: chats
       })
     }
 
