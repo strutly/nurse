a548c4fd17a97f248ef140e5af316d7479bca001
strutly <290616329@qq.com>
2024-08-16 16:11:29 +0800

    update

--- pages/diabetes/patient/edit.js
+++ pages/diabetes/patient/edit.js
@@ -1,66 +1,114 @@
-// pages/diabetes/patient/edit.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
+var that;
+import Api from '../../../config/api';
+import CustomPage from '../../../CustomPage';
+CustomPage({
   data: {
-
+    info:{},
+    sourceArr:['手工填写','艾糖CGM','瞬感CGM'],
+    allline:{},
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
+  async onLoad(options) {
+    console.log(options)
+    that = this;
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
+  onShow(){
+    Api.detailPatient({
+      id:that.data.options.id
+    }).then(res=>{
+      that.setData({
+        info:res.data.patient,
+        nums:res.data.nums
+      });
+    },err=>{
+      that.showTips(err.msg);
+    })
+  },  
+  showAll(e){
+    console.log(e);
+    let type = e.currentTarget.dataset.type;
+    let allline = that.data.allline;
+    allline[type] = !allline[type];
+    that.setData({
+      allline:allline
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
+  pickerChange(e){
+    console.log(e);
+    let name = e.currentTarget.dataset.name;
+    let info = that.data.info;
+    info[name] = e.detail.value;
+    that.setData({
+      info:info
+    })
+    
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
+  edit(e){
+    console.log(e);
+    let dataset = e.currentTarget.dataset;
+    let patientData = that.data.info;
+    dataset['data'] = patientData.scanData[dataset['name']];
+    that.setData({
+      modaledit:true,
+      editData:dataset
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
+  addEdit(){
+    let editData = that.data.editData;
+    editData.data.push("");
+    that.setData({
+      editData:editData
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
+  editRemove(e){
+    console.log(e)
+    let index = e.currentTarget.dataset.index;
+    let editData = that.data.editData;
+    editData.data.splice(index, 1);
+    that.setData({
+      editData: editData
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
+  editEnd(e){
+    console.log(e);
+    let index = e.currentTarget.dataset.index;
+    let type = e.currentTarget.dataset.type;
+    let editData = that.data.editData;
+    if(type=='list'){
+      editData['data'][index] = e.detail.value;
+    }else{
+      editData['data'] = e.detail.value;
+    }
+    that.setData({
+      editData:editData
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  save(e){
+    console.log(e);
+    let name = e.currentTarget.dataset.name;
+    let editData = that.data.editData;
+    let data = editData.data;
+    let type = e.currentTarget.dataset.type;
+    if(type=='list'){
+      data = data.filter(item=>item.trim().length>0);
+    }    
+    console.log(data);
+    let patientData = that.data.info;
+    patientData.scanData[name] = data;
+    that.setData({
+      modaledit:false,
+      info:patientData
+    })
+  },
+  submit(e){
+    let info = that.data.info;
+    Api.updatePatient(JSON.stringify(info)).then(res=>{
+      console.log(res);
+      that.showTips("保存成功","success");
+      setTimeout(() => {
+        wx.navigateBack();
+      }, 1500);
+    },err=>{
+      console.log(err);
+    })
   }
 })
